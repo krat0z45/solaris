d324a45a1d30c616e825e405f74927a807eb1bb5
krat0z45 <motokrat0z@gmail.com>
2025-10-17 09:14:40 -0600

    LOGIN

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -16,9 +16,9 @@ export default function LoginPage() {
         <div className="w-full max-w-4xl mx-auto bg-white rounded-2xl shadow-2xl grid lg:grid-cols-2 overflow-hidden">
           <div className="relative h-full hidden lg:flex flex-col items-center justify-center bg-destructive p-8 text-destructive-foreground">
               <Image
-                  src="https://pouch.jumpshare.com/preview/G1a6Vmh3RjT3RzXc19A1E0Y4dfpP0r-396gsltrv2-l8y4F8V6rR9P5E8Y1jZpB5S_3-4Jv-Z-B6j5-Bf_hY2l8mYv_E.png"
-                  width={150}
-                  height={150}
+                  src="/acciona_login.png"
+                  width={300}
+                  height={300}
                   alt="Company Logo"
                   className="invert"
                   data-ai-hint="logo"
